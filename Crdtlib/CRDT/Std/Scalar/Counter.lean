6de7be65960fb995f95f7b8c4eafa2b06c1f8b21
Alexander Städing <alex@staeding.com>
2026-05-29 13:52:01 +0200

    Fix Counter compile error from rename

--- Crdtlib/CRDT/Std/Scalar/Counter.lean
+++ Crdtlib/CRDT/Std/Scalar/Counter.lean
@@ -1,17 +1,17 @@
 import Crdtlib.CRDT.Primitive.ACFun
 
-def counter64' : CRDT' Int64 Int64 Int64 := ac_fun' Int64 (· + ·)
+def counter64 : CRDT Int64 Int64 Int64 := ac_fun Int64 (· + ·)
 
 @[export counter64_effect]
 def counter64Effect (event : Int64) (state : Int64) : Int64 :=
-  counter64'.effect event state
+  counter64.effect event state
 
 @[export counter64_interpret]
 def counter64Interpret (state : Int64) : Int64 :=
-  counter64'.interpret state
+  counter64.interpret state
 
 
-def counter32' : CRDT' Int32 Int32 Int32 := ac_fun' Int32 (· + ·)
+def counter32' : CRDT Int32 Int32 Int32 := ac_fun Int32 (· + ·)
 
 @[export counter32_effect]
 def counter32Effect (event : Int32) (state : Int32) : Int32 :=
