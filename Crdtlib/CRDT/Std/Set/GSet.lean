ee3e63e47c6559c593f8de32ee77d21b40222928
Alexander Städing <alex@staeding.com>
2026-07-22 02:26:28 -0400

    Clean up GSet

--- Crdtlib/CRDT/Std/Set/GSet.lean
+++ Crdtlib/CRDT/Std/Set/GSet.lean
@@ -7,21 +7,16 @@ import Crdtlib.Combinator.Derived.MapOp
 
 section GSet
 
-variable (σ : Type*) [Hashable σ] [DecidableEq σ]
+abbrev GSet (σ : Type*) [Hashable σ] [DecidableEq σ] := Associate σ ℕ
 
-abbrev GSet := Std.ExtHashMap σ ℕ
-
-instance : Zero $ GSet σ where
+instance (σ : Type*) [Hashable σ] [DecidableEq σ] : Zero $ GSet σ where
   zero := Std.ExtHashMap.emptyWithCapacity 8
 
-def gset
-  : CRDT σ (GSet σ) (SetInterp σ)
-  := map_op (λ x ↦ ⟨x, 1⟩)
-    $ map_interpretation (λ s ↦ {
-        mem := (· > 0) ∘ s.map
-        toList := s.toList.filterMap λ ⟨k, v⟩ ↦ if v > 0 then .some k else .none
-      })
-    $ associate σ gcounter
+def gset (σ : Type*) [Hashable σ] [DecidableEq σ] : CRDT σ (GSet σ) (SetInterp σ)
+  := map_interpretation
+      (λ s ↦ { mem := (· > 0) ∘ s.map, toList := s.toList.filterMap λ ⟨k, v⟩ ↦ if v > 0 then .some k else .none })
+      (map_op (λ x ↦ ⟨x, 1⟩)
+        (associate σ gcounter))
 
 section GSetFFI
 
