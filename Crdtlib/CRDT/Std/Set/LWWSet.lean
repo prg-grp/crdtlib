98ac659bc5df9e0877ed106838d502886a1f5248
Alexander Städing <alex@staeding.com>
2026-08-18 01:25:20 -0400

    Pass on lww_element_set

--- Crdtlib/CRDT/Std/Set/LWWSet.lean
+++ Crdtlib/CRDT/Std/Set/LWWSet.lean
@@ -5,9 +5,12 @@ import Crdtlib.Combinator.Derived.DisjointProduct
 import Crdtlib.CRDT.Std.Set.GSet
 import Mathlib.Data.Finset.Basic
 
-def lww_element_set [PartialOrder τ] (σ : Type) [DecidableEq τ] [DecidableEq σ] [DecidableLT τ]
-  : CRDT (SetOp (Event τ σ)) (Finset (Event τ σ) × Finset (Event τ σ)) (Finset σ)
-  := map_interpretation (λ ⟨addSet, removeSet⟩ ↦
-      (addSet.filter (λ a ↦ ¬ (removeSet.filter (λ r ↦ r.o = a.o ∧ a.t < r.t)).Nonempty)).image (·.o))
-    (map_op (λ | .add elem => .inl {elem} | .remove elem => .inr {elem})
-      (disjoint_product (union_set (Event τ σ)) (union_set (Event τ σ))))
+def lww_element_set (σ : Type) [PartialOrder τ] [DecidableLT τ] [DecidableEq τ] [Hashable τ]
+    [DecidableEq σ] [Hashable σ] [LinearOrder (Event τ σ)] :
+    CRDT (SetOp (Event τ σ)) (GSet (Event τ σ) × GSet (Event τ σ)) (SetInterp σ) :=
+  map_interpretation (λ ⟨addSet, removeSet⟩ ↦
+      let removes := removeSet.toList
+      let live := addSet.toList.filter (λ a ↦ ¬ removes.any (λ r ↦ r.o == a.o ∧ (a.t < r.t)))
+      { mem s := live.any (λ a ↦ a.o == s), toList := (live.map (·.o)).dedup })
+    (map_op (λ | .add elem => .inl elem | .remove elem => .inr elem)
+      (disjoint_product (gset (Event τ σ)) (gset (Event τ σ))))
