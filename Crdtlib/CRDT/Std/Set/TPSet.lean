5e0aef3a811ce89b51e9e2afd9894c4088e7deee
Alexander Städing <alex@staeding.com>
2026-08-18 02:13:07 -0400

    Clean up tpset

--- Crdtlib/CRDT/Std/Set/TPSet.lean
+++ Crdtlib/CRDT/Std/Set/TPSet.lean
@@ -6,21 +6,12 @@ import Crdtlib.Combinator.Derived.MapOp
 
 section TPSet
 
-variable (σ : Type*) [Hashable σ] [DecidableEq σ]
-
-def TPSetState := (GSet σ × GSet σ)
-
-def tpset [DecidableEq σ] [Hashable σ] [Zero σ] : CRDT (SetOp σ) (TPSetState σ) (SetInterp σ) :=
-  map_interpretation
-    (λ ⟨addSet, removeSet⟩ ↦ {
-      mem k := addSet.mem k ∧ ¬ removeSet.mem k
-      toList := addSet.toList.filter (¬ removeSet.mem ·)
-    })
-    (map_op
-      (λ op ↦ match op with
+def tpset (σ : Type*) [DecidableEq σ] [Hashable σ] [Zero σ] : CRDT (SetOp σ) (GSet σ × GSet σ) (SetInterp σ) :=
+  map_interpretation (λ ⟨addSet, removeSet⟩ ↦ {
+      mem k := addSet.mem k ∧ ¬ removeSet.mem k, toList := addSet.toList.filter (¬ removeSet.mem ·) })
+    (map_op (λ
         | .add    elem => .inl elem
         | .remove elem => .inr elem)
-      (disjoint_product (gset σ) (gset σ))
-    )
+      (disjoint_product (gset σ) (gset σ)))
 
 def tpsetₜ [PartialOrder τ] (σ : Type) [Hashable σ] [Zero σ] [DecidableEq σ] := (tpset σ).toCRDTₜ τ
